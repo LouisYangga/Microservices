fbb12cc10ca96ff8d6b7a1180f06511ad16d9cc6
LouisYangga <102441218+LouisYangga@users.noreply.github.com>
2023-10-17 00:04:13 +1100

    move db to cloud

--- Backend/AdviceService/db/db.js
+++ Backend/AdviceService/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://127.0.0.1/advice';
+const url = 'mongodb+srv://microservices:microservices123@microservices.shsnvxr.mongodb.net/';
 const connectionParams = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
